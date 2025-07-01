7b44670c06ae84a99fb31b1f3d2b970e7dff3155
milosmarinkovic1 <75090508+milosmarinkovic1@users.noreply.github.com>
2025-07-01 13:44:54 +0200

    notes

--- src/main/java/rs/ac/singidunum/programskijezici2025/controller/PlanTreningaController.java
+++ src/main/java/rs/ac/singidunum/programskijezici2025/controller/PlanTreningaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController  // vraca nam json odgovore umesto html view-a
 @RequestMapping("/api/planovi")
 public class PlanTreningaController {
 
